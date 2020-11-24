55c3bbd3ad0c7dbe0d14438c03a35f86ebf15024
Perrine <73954261+pacloef@users.noreply.github.com>
2020-11-24 09:29:54 +0100

    Update App.js

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,6 +4,8 @@
 //  This source code is licensed under the license found in the
 //  LICENSE file in the root directory of this source tree.
 
+yarn upgrade
+
 import React from "react";
 import { BoardContext } from "./BoardState";
 import Overlay from "./components/Overlay";
